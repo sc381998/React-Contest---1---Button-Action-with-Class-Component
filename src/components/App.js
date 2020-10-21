5ac48e1b5352b006695f2cf721f714e80c1424af
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-21 21:19:37 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,16 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { para: "" };
+    this.state = { id: "", para: "" };
   }
+  // useState = {
+  //   id: "",
+  //   para: ""
+  // };
+
   handleClick = () => {
     this.setState({
+      id: "para",
       para:
         "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
     });
@@ -16,8 +22,8 @@ class App extends Component {
   render() {
     return (
       <div id="main">
-        <button onClick={this.handleClick}></button>
-        <p>{this.state.para}</p>
+        <button onClick={this.handleClick}>Click</button>
+        <p id={this.useState.id}>{this.useState.para}</p>
       </div>
     );
   }
